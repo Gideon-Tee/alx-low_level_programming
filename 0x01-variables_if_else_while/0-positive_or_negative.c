ab97cb47d3a6d51eea9ffd473f9d189c6a8de4ce
Gideon <gideonlambride@gmail.com>
2023-02-17 13:20:12 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
 /** 
- * the main function contains the logic for the program
- *
+ * main - main function, contains the code logic
+ * description: print the state of a randomly generated number
+ * return 0
  */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
