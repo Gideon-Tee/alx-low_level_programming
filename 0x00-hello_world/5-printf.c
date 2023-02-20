e4346e8c0a80b1d84b41077674a74f465f0c3284
Gideon <gideonlambride@gmail.com>
2023-02-20 03:08:10 +0000

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 
 /**
  * main - Entry point
- * return: Returns (0) on success
+ * Description: print output to the screen
+ * Return: Returns (0) on success
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
