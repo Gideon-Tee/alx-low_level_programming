c1a661e561cbb6459bff49617d62d9bd5fa2cdce
Gideon <gideonlambride@gmail.com>
2023-02-20 02:12:34 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * Description: Print to standard output
+ * Description - Print to standard output
+ * main - contains the logic
  * Return: Return 0 when successful
  */
 
-int main()
+int main(void)
 {
 	char text[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(text);
 	return (0);
 }
