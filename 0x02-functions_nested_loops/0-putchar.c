b12efbd9519a7ef60d21c1cbc578f2cf122b9959
Gideon <gideonlambride@gmail.com>
2023-02-22 01:12:40 +0000

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,7 @@
  * main - print an output to the std out
  * _putchar - print a character
  * description: use the putchar() function for printing to std out
- * return: return 0 when successful
+ * Return: 0 when successful
  */
 
 int main(void)
