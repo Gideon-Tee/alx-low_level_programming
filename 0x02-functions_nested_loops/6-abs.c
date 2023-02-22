2c4276580e54798e6340e558583391afadd15612
Gideon <gideonlambride@gmail.com>
2023-02-22 03:34:05 +0000

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdlib.h>
+
+/**
+ * _abs - function block
+ *
+ *@a - value to be processed
+ *
+ * Return: absolute value
+ */
+
+int _abs(int a)
+{
+	return abs(a);
+}
