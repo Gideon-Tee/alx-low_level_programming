8b99c649e957fd805016309b285c1381b1248e43
Gideon <gideonlambride@gmail.com>
2023-02-23 22:21:26 +0000

    0-main.c updated

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,15 +2,15 @@
 
 /**
  * main - tests function positive_or_negative()
- * positive_or_negative() - function to be tested
  * Return: returns 0
  */
 
 int main(void)
 {
-	int n = 0;
+	int i;
 
-	positive_or_negative(n);
+	i = 0;
+	positive_or_negative(i);
 
 	return (0);
 }
