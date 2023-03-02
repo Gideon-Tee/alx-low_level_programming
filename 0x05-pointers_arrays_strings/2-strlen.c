75bd34989a5ce5ffb5631c458af0a79262044151
Gideon <gideonlambride@gmail.com>
2023-03-02 16:56:09 +0000

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of string
+ * @s: string to be used
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	return strlen(*s);
+}
