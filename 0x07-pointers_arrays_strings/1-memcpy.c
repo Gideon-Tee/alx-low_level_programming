da39d1dbee45594eb755b61663fd1e81c4636edc
Gideon <gideonlambride@gmail.com>
2023-03-05 22:11:59 +0000

    Final update to 1-memcpy.c to meet betty coding style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,8 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _memcpy - copies bytes from one memory area to another.
+ * _memcpy - copies memory area
  * @dest: destination memory area.
  * @src: source memory area.
  * @n: number of bytes to be copied.
