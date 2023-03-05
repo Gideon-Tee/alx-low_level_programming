7fbf7f66f10f49d1b51c61d72ddc19db6941c533
Gideon <gideonlambride@gmail.com>
2023-03-05 22:47:37 +0000

    Final update to 1-memcpy.c to meet betty coding style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,22 @@
-#include "main"
+#include "main.h"
 #include <stdio.h>
 
 /**
- * _memcpy - copies memory area
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: number of bytes to be copied.
+ **_memcpy - function that copies memory area.
+ * @dest: pointer to the object to copy to.
+ * @src: pointer to the object to copy from.
+ * @n: number of bytes to copy.
  *
- * Return: the pointer dest
+ * Return:  a pointer dest or 0 on success.
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	i = 0;
-
 	for (i = 0; i < n; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
-
 	return (dest);
 }
