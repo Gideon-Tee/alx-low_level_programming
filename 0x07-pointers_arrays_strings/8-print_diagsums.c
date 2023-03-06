ca5d6f7dca96bfe29fa5f38fcb216ae9abb5d128
Gideon <gideonlambride@gmail.com>
2023-03-06 23:46:34 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,36 +2,27 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - prints the sums of matrix diagonals
- * @a: pointer to the matrix
- * @size: size of the square matrix
+ * print_diagsums - function that prints sum of the two diagonals,
+ * of a square matrix of integers.
+ * @a: pointer to array of characters.
+ * @size: size of array.
  *
- * Return: Nothing
+ * Return: no return.
  */
 
 void print_diagsums(int *a, int size)
 {
-	unsigned int i, j, k, l;
-	int sum1 = 0;
-	int sum2 = 0;
+	int i, max_size;
+	int prinDiagonalSum = 0;
+	int secDiagonalSum = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-				sum1 += *a[i][j];
-		}
-	}
+	max_size = size * size;
 
-	for (k = size - 1; k >= 0; k--)
-	{
-		for (l = size - 1; l >= 0; l--)
-		{
-			if (k == l)
-				sum2 += *a[k][l];
-		}
-	}
+	for (i = 0; i < max_size; i += size + 1)
+		prinDiagonalSum += a[i];
 
-	printf("%d, %d", sum1, sum2);
+	for (i = size - 1; i < max_size - 1; i += size - 1)
+		secDiagonalSum += a[i];
+
+	printf("%d, %d\n", prinDiagonalSum, secDiagonalSum);
 }
