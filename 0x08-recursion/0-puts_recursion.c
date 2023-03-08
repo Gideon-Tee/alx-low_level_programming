d39adc7a11eb3fcc5c9cfad7b6fddf539c609831
Gideon <gideonlambride@gmail.com>
2023-03-08 22:39:24 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,8 +4,8 @@
 void _puts_recursion(char *s)
 {
 	if (strlen(s) == 0)
-		_putchar('');
+		_putchar('\n');
 
-	_putchar(s[0]) + _puts_recursion(s + 1);
-	_putchar('\n');
+	_putchar(*s[0]);
+	_puts_recursion(s + 1);
 }
