0d9aa4684d33c597948d8ea173b6712020ce7f8f
Gideon <gideonlambride@gmail.com>
2023-03-08 22:45:09 +0000

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,17 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _puts_recursion - prints string
+ * @s: pointer to the string to print
+ * Return: Nothing
+ */
+
 void _puts_recursion(char *s)
 {
 	if (strlen(s) == 0)
 		_putchar('\n');
 
-	_putchar(*s);
+	_putchar(s[0]);
 	_puts_recursion(s + 1);
 }
