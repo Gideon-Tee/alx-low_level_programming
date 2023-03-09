af72e2563db8184bce014e5b9228f42921723133
Gideon <gideonlambride@gmail.com>
2023-03-09 01:11:09 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,13 +10,14 @@ int _sqrt_recursion(int n)
 {
 	if (n == 1)
 		return (1);
-	else if (n == 0)
+	if (n == 0)
 		return (0);
-	else if (n < 0)
+	if(n < 0)
 		return (-1);
-	else
-		return (_square_root(n, 0));
 
+	return (_square_root(n, 0));
+
+}
 /**
  * _square_root - calculates square root
  * @n: number
