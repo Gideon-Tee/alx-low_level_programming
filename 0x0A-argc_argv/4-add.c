867ec5b32f7e7e65d8345e4b7e5c8e38c68fd1d7
Gideon <gideonlambride@gmail.com>
2023-03-11 10:19:40 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,6 +3,13 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+ * main - adds numbers
+ * @argc: number of parameters
+ * @argv: all parameters
+ * return: 0 when successful
+ */
+
 int main(int argc, char *argv[])
 {
 	int i, result = 0;
@@ -13,7 +20,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc - 1; i++)
 		{
-			if (isdigit(argv[i] == 0)
+			if (isdigit(argv[i]) == 0)
 			{
 				printf("Error\n");
 				return (1);
