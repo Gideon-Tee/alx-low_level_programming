e17476056cfaec2faf04d91df2715bf5befa58b7
Gideon <gideonlambride@gmail.com>
2023-03-20 20:04:41 +0000

    Updated dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of the dog
- * Description: This creates a dog structure and gives it the attributes provided
  */
 
 struct dog
@@ -14,6 +13,6 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
