543447ac79bf028ec137b0565ed7fc5e34c4a555
Gideon <gideonlambride@gmail.com>
2023-03-20 20:00:53 +0000

    Header file dog.h to contain dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ * Description: This creates a dog structure and gives it the attributes provided
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
