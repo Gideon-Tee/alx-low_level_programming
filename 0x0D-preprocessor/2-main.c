21e0c96e686e1da3ae8331be5de9a799bdf8f8e3
Gideon <gideonlambride@gmail.com>
2023-03-21 00:21:57 +0000

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the name of it's file
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
