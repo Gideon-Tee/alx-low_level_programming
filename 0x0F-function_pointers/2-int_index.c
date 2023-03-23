30ab96f834f554a247bedcd0f9a591bf87afacc7
Gideon <gideonlambride@gmail.com>
2023-03-23 00:34:42 +0000

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_headers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search through
+ * @size: size of the array
+ * @cmp: pointer to function to compare values
+ * Return: index of item or -1 if unsuccessful
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	unsigned int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
