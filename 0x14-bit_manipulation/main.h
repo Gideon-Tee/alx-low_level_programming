54b08230f075e328efb5fcae9ebd0bab18e3e921
Gideon <gideonlambride@gmail.com>
2023-04-10 01:28:30 +0000

    1-print_binary.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-int _putchar(char *c);
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
 void print_binary(unsigned long int n);
