e94b807d7e28559bf897f4721521572eb48ff318
Gideon <gideonlambride@gmail.com>
2023-05-12 22:05:27 +0000

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -13,5 +16,7 @@ int _stringlen(const char *s);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
+
+
 
 #endif
