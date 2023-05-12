8e6f99966fb77b60b5a503fcbbfbf58d18c544b8
Gideon <gideonlambride@gmail.com>
2023-05-12 22:20:38 +0000

    Update to fix betty-style

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,6 +17,11 @@ int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
 
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 
 #endif
