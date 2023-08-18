78fabd8df2793aec802d8e053b504aea26e36708
Gideon Tettehfio <gideonlambride@gmail.com>
2023-08-17 20:27:59 -0700

    Compilation error fixed

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -9,18 +9,6 @@
  * @n: integer
  * @prev: points to the previous node
  * @next: points to the next node
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the previous node
- * @next: points to the next node
- *
  * Description: doubly linked list node structure
  * for ALX project
  */
