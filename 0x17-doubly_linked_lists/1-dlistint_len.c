1adcd9ad0a62f6262dd4a580cd337c4212cc7023
Gideon Tettehfio <gideonlambride@gmail.com>
2023-08-17 22:05:47 -0700

    Bug Fixed

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,13 +5,6 @@
  * a double linked list
  *
  * @h: head of the list
-#include "lists.h"
-
-/**
- * dlistint_len - returns the number of elements in
- * a double linked list
- *
- * @h: head of the list
  * Return: the number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
